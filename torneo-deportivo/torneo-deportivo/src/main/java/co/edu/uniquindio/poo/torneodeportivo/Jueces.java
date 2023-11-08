88d0f09fbf8998363965a79cfb2272623f65251d
Daniel Jurado <daniele.juradoc@uqvirtual.edu.co>
2023-11-08 11:39:05 -0500

    jueces

--- torneo-deportivo/torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Jueces.java
+++ torneo-deportivo/torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Jueces.java
@@ -1,5 +1,9 @@
 package co.edu.uniquindio.poo.torneodeportivo;
 
 public class Jueces {
-    
+    private String licenciaJues;
+    private String nombre;
+    private String apellido;
+    private String email;
+
 }
