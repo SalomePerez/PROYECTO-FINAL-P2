329c0eafa4a576accd3e372e7d8bd10148fec6c1
SalomePerez <salome.perezf@uqvirtual.edu.co>
2023-11-08 11:56:38 -0500

    extensión de la clase jueces hecha

--- torneo-deportivo/torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Jueces.java
+++ torneo-deportivo/torneo-deportivo/src/main/java/co/edu/uniquindio/poo/torneodeportivo/Jueces.java
@@ -1,5 +1,20 @@
 package co.edu.uniquindio.poo.torneodeportivo;
 
-public class Jueces {
-    
+public class Jueces extends Persona{
+
+    private String licenciaDeJuez;
+
+    public Jueces(String nombre, String apellido, String email, String celular, String licenciaDeJuez) {
+        super(nombre, apellido, email, celular);
+        this.licenciaDeJuez = licenciaDeJuez;
+    }
+
+    public String getLicenciaDeJuez() {
+        return licenciaDeJuez;
+    }
+
+    public void setLicenciaDeJuez(String licenciaDeJuez) {
+        this.licenciaDeJuez = licenciaDeJuez;
+    }
+
 }
